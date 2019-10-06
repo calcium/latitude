a111e92e0979fb3e753e4e2ad26d2c2b9c88f198
Chai Ang <github@pyuser.com>
2019-10-07 00:20:29 +1100

    made var naming consistent

--- src/main/java/com/pyuser/latitude/Latitude_Exercise_1.java
+++ src/main/java/com/pyuser/latitude/Latitude_Exercise_1.java
@@ -25,15 +25,28 @@ public class Latitude_Exercise_1 {
         return bestEnd;
     }
 
-
+    /**
+     * Algorithm :
+     * As we traverse the array, we keep the max difference ie. bestResult.
+     * If there is a new low share price encountered, we mark it as the current low,
+     * using this new current low to calculate subsequent results, keeping the
+     * results if it betters the bestResult value.
+     * @see Jadane's Algorithm.
+     * @see https://en.wikipedia.org/wiki/Maximum_subarray_problem#Kadane's_algorithm
+     *
+     * @param sharePrices
+     * @return bestResult
+     * @throws InvalidValueException
+     * @throws InsufficientDataException
+     */
     public int get_max_profit(int[] sharePrices) throws InvalidValueException, InsufficientDataException {
         int numSharePrices = sharePrices.length;
         if (numSharePrices < 2) {
             throw new InsufficientDataException(String.format("No data in provided list."));
         }
 
-        int current_diff = 0;
-        int current_start = 0;
+        int currentDiff = 0;
+        int currentStart = 0;
 
         for (int idx = 0; idx < numSharePrices; ++idx) {
             int sharePrice = sharePrices[idx];
@@ -41,16 +54,15 @@ public class Latitude_Exercise_1 {
                 throw new InvalidValueException(String.format("Invalid share price at position(%d)", idx));
             }
             // new low for the day. mark it.
-            if (sharePrice < sharePrices[current_start]) {
-                current_start = idx;  // dont do anything else.
+            if (sharePrice < sharePrices[currentStart]) {
+                currentStart = idx;  // dont do anything else.
             } else {
-//                # Extend the existing sequence with the current element
-                current_diff = sharePrice - sharePrices[current_start];
+                currentDiff = sharePrice - sharePrices[currentStart];
             }
 
-            if (current_diff > bestResult) {
-                bestResult = current_diff;
-                bestStart = current_start;
+            if (currentDiff > bestResult) {
+                bestResult = currentDiff;
+                bestStart = currentStart;
                 bestEnd = idx;
             }
         }
